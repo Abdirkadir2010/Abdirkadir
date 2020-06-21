45f7d06716e033b0c24b3565137bfe623f29b3be
Abdirkadir <66696816+Abdirkadir2010@users.noreply.github.com>
2020-06-21 17:54:44 +0300

    Update Student_grading_System.java

--- Student_grading_System.java
+++ Student_grading_System.java
@@ -1,5 +1,24 @@
 
-Student Total course average
-rank
-transcript
-All information
+public class studentGradingSystem{
+  
+Total_course(){
+  return total;
+}
+rank(){
+  return srank;
+}
+transcript()
+{
+  return scard;
+}
+All_Information()
+{
+  return info;
+}
+  public static void main(String[] args);{
+    rank();
+    transcript();
+    All_Information()
+    public ststic System.out("STUDENT GRADING SYSTEM);
+  }
+}
